4eed2cda2b258d18d85a4bf559cc7f897e3c241a
传人 <128107971+user134545@users.noreply.github.com>
2024-04-05 11:18:27 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,80 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+from statistics import mode
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
+from sklearn.naive_bayes import GaussianNB
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, confusion_matrix
+
+DATA_PATH = r""
+data = pd.read_csv(DATA_PATH).dropna(axis=1)
+
+encoder = LabelEncoder()
+data["prognosis"] = encoder.fit_transform(data["prognosis"])
+X = data.iloc[:, :-1]
+y = data.iloc[:, -1]
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=24)
+
+svm_model = SVC()
+svm_model.fit(X_train, y_train)
+
+nb_model = GaussianNB()
+nb_model.fit(X_train, y_train)
+
+rf_model = RandomForestClassifier(random_state=18)
+rf_model.fit(X_train, y_train)
+
+symptoms = X.columns.values
+symptom_index = {}
+for index, value in enumerate(symptoms):
+    symptom = " ".join([i.capitalize() for i in value.split("_")])
+    symptom_index[symptom] = index
+
+data_dict = {
+    "symptom_index": symptom_index,
+    "predictions_classes": encoder.classes_
+}
+
+
+def predictDisease(symptoms):
+    symptoms = [symptom.capitalize() for symptom in symptoms.split(",")]  # Format the symptoms
+
+    input_data = [0] * len(data_dict["symptom_index"])
+    for symptom in symptoms:
+        index = data_dict["symptom_index"].get(symptom, -1)  # Use get() to avoid KeyError
+        if index != -1:
+            input_data[index] = 1
+
+    input_data = np.array(input_data).reshape(1, -1)
+
+    rf_prediction = data_dict["predictions_classes"][rf_model.predict(input_data)[0]]
+    nb_prediction = data_dict["predictions_classes"][nb_model.predict(input_data)[0]]
+    svm_prediction = data_dict["predictions_classes"][svm_model.predict(input_data)[0]]
+
+    final_prediction = mode([rf_prediction, nb_prediction, svm_prediction])
+    predictions = {
+        "rf_model_prediction": rf_prediction,
+        "naive_bayes_prediction": nb_prediction,
+        "svm_model_prediction": svm_prediction,
+        "final_prediction": final_prediction
+    }
+    return predictions
+
+
+def main():
+    st.title("Disease Prediction App")
+    symptoms_input = st.text_input("Enter symptoms separated by commas:")
+
+    if st.button("Predict"):
+        predictions = predictDisease(symptoms_input)
+        st.write(f"Predicted Disease: {predictions['final_prediction']}")
+
+
+if __name__ == "__main__":
+    main()
